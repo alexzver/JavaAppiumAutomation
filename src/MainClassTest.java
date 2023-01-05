b6bcd4f61d0771556da3a64841f03ab250ab3e50
alexzver <alexzver8@gmail.com>
2023-01-05 19:59:18 +0300

    Test3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,9 +1,10 @@
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
     @Test
-    public void testGetClassNumber(){
-        Assert.assertTrue("Class number less than 45",MainClass.getClassNumber() > 45);
+    public void testGetClassString(){
+        Assert.assertTrue("Substring is not found", MainClass.getClassString().contains("Hello") || MainClass.getClassString().contains(("hello")));
     }
 }
